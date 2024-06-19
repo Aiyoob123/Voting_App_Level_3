24d4ffe152c94660fe3416328446f9a036d61b05
Aiyoob <aiyoobbhaiyaat@gmail.com>
2024-06-19 11:21:15 +0200

    update_all

--- voting_app/models.py
+++ voting_app/models.py
@@ -5,6 +5,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Below is aclass called 'Question'
+# This Python class represents a model for a question with text and publication date attributes.
+# This Python class defines a model for a question with text and publication date attributes.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -13,6 +15,8 @@ class Question(models.Model):
         return self.question_text
 
 # Below is aclass called 'Choice'
+# This class represents a choice for a question in a Python Django model, with attributes for the
+# choice text and number of votes.
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
